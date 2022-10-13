aa5ffd1c42c16c735c8960551acab38f9e36befb
TQHuy <tqhuy16@gmail.com>
2022-10-13 20:08:02 +0700

    deploy app to surge

---
+++ deploy-surge.sh
@@ -0,0 +1,11 @@
+# Build react app with production mode
+npm run build
+
+# Move to build folder
+cd build
+
+# Clone index.html into 200.html
+cp index.html 200.html
+
+# Deploy command
+surge . tqhuy-nals.surge.sh
